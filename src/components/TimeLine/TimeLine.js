51868f9b0f5402f724f18e33a6e4d272cb750cfa
AllenZZP <131452abc@gmail.com>
2022-03-10 13:25:58 -0800

    implement a lot

--- src/components/TimeLine/TimeLine.js
+++ src/components/TimeLine/TimeLine.js
@@ -7,45 +7,88 @@ import { TimeLineData } from '../../constants/constants';
 const TOTAL_CAROUSEL_COUNT = TimeLineData.length;
 
 const Timeline = () => {
-  // const [activeItem, setActiveItem] = useState(0);
-  // const carouselRef = useRef();
+  const [activeItem, setActiveItem] = useState(0);
+  const carouselRef = useRef();
 
-  // const scroll = (node, left) => {
-  //   return node.scrollTo({ left, behavior: 'smooth' });
-  // }
+  const scroll = (node, left) => {
+    return node.scrollTo({ left, behavior: 'smooth' });
+  }
 
-  // const handleClick = (e, i) => {
-  //   e.preventDefault();
+  const handleClick = (e, i) => {
+    e.preventDefault();
 
-  //   if (carouselRef.current) {
-  //     const scrollLeft = Math.floor(carouselRef.current.scrollWidth * 0.7 * (i / TimeLineData.length));
+    if (carouselRef.current) {
+      const scrollLeft = Math.floor(carouselRef.current.scrollWidth * 0.7 * (i / TimeLineData.length));
       
-  //     scroll(carouselRef.current, scrollLeft);
-  //   }
-  // }
+      scroll(carouselRef.current, scrollLeft);
+    }
+  }
 
-  // const handleScroll = () => {
-  //   if (carouselRef.current) {
-  //     const index = Math.round((carouselRef.current.scrollLeft / (carouselRef.current.scrollWidth * 0.7)) * TimeLineData.length);
+  const handleScroll = () => {
+    if (carouselRef.current) {
+      const index = Math.round((carouselRef.current.scrollLeft / (carouselRef.current.scrollWidth * 0.7)) * TimeLineData.length);
 
-  //     setActiveItem(index);
-  //   }
-  // }
+      setActiveItem(index);
+    }
+  }
 
-  // // snap back to beginning of scroll when window is resized
-  // // avoids a bug where content is covered up if coming from smaller screen
-  // useEffect(() => {
-  //   const handleResize = () => {
-  //     scroll(carouselRef.current, 0);
-  //   }
+  // snap back to beginning of scroll when window is resized
+  // avoids a bug where content is covered up if coming from smaller screen
+  useEffect(() => {
+    const handleResize = () => {
+      scroll(carouselRef.current, 0);
+    }
 
-  //   window.addEventListener('resize', handleResize);
-  // }, []);
+    window.addEventListener('resize', handleResize);
+  }, []);
 
   return (
-    <div>
-      Timeline
-    </div>
+    <Section id="about">
+      <SectionTitle>Timeline</SectionTitle>
+      <SectionText>
+        Below is my milestone when I start learning coding.
+      </SectionText>
+
+      <CarouselContainer ref={ carouselRef} onScroll={ handleScroll }>
+        <>
+          {TimeLineData.map((item, i) => (
+            <CarouselMobileScrollNode key={ i } final={ i === TOTAL_CAROUSEL_COUNT - 1}>
+              <CarouselItem 
+                index={ i }
+                id={`carousel__item-${i}`}
+                active={ activeItem }
+                onClick={ (e) => handleClick(e, i)}
+              >
+                <CarouselItemTitle>
+                  { item.year}
+                </CarouselItemTitle>
+
+                <CarouselItemText>
+                  { item.text }
+                </CarouselItemText>
+
+
+
+              </CarouselItem>
+            </CarouselMobileScrollNode>
+          ))}
+        </>
+      </CarouselContainer>
+      <CarouselButtons>
+        {TimeLineData.map((item, i) => (
+          <CarouselButton
+            key={ i }
+            index={ i }
+            active={ activeItem}
+            onClick={ (e) => handleClick(e, i)}
+            type="button"
+          >
+            <CarouselButtonDot active={ activeItem } />
+          </CarouselButton>
+        ))}
+      </CarouselButtons>
+      <SectionDivider />
+    </Section>
   );
 };
 
