d26285c5a6685408415637895242febddce5527d
AllenZZP <131452abc@gmail.com>
2022-03-10 12:02:45 -0800

    implement hero section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,10 +4,20 @@ import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponent
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome To <br />
+        Allen's Personal Portfolio
+      </SectionTitle>
+      <SectionText>
+        I am passionate about developing apps that allow users to connect and learn from each other.
+      </SectionText>
+      <Button onClick={ () => window.open("https://resume.creddle.io/resume/922r3etvr20", '_blank')}>Learn More
+      </Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
